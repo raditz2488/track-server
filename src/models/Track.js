b80ef719b134ba9ebd8a52a88316efd2b5b5316f
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-07 23:44:24 +0530

    Defined the schema for point.

--- src/models/Track.js
+++ src/models/Track.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
+const pointSchema = new mongoose.Schema({
+    timestamp: Number,
+    coords: {
+        latitude: Number,
+        longitude: Number,
+        altitude: Number,
+        accuracy: Number,
+        heading: Number,
+        speed: Number
+    }
+})
+
 const trackSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
