f78c43469dfcad677e85a1e9a9d9726df1b11c5c
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-08 00:29:53 +0530

    Added check for post tracks for the name and locations.

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -17,6 +17,11 @@ router.get("/tracks", async (req, res) => {
 
 router.post("/tracks", async (req, res) => {
     const { name, locations } = req.body;
+
+    if (!name || !locations) {
+        res.status(422).send({ error: "You must provide name and locations." })
+    }
+
     try {
         const track = new Track({ name, locations, userId: req.user._id });
         await track.save();
