77ded16ebf880291d44c8e37c6f7060c07b5f947
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-07 21:16:12 +0530

    Imported bcrypt into User.js. Also written is bare bones implementation for pre on save, which just checks if password is not modified then return.

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -10,6 +11,15 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+userSchema.pre("save", function() {
+    const user = this;
+    if (!user.isModified("password")) {
+        return
+    }
+
+
 });
 
 mongoose.model("User", userSchema);
