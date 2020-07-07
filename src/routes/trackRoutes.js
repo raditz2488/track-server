8e1476c680e1acdfbec4e057a7f1ab9d2ed6d5a0
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-08 00:00:05 +0530

    Got reference to the Track schema.

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -2,4 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requiresAuth = require("../middlewares/requiresAuth");
 
+const trackSchema = mongoose.model("Track");
+
 const router = express.Router();
