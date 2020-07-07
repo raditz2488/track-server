8f40d6884a4f9f7b1b0091b6b55c4c7ff896aa12
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-07 23:40:35 +0530

    Corrected the error of not using new.

--- src/models/Track.js
+++ src/models/Track.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const trackSchema = mongoose.Schema({
+const trackSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
