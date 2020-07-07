41bcb0222aa85ca70737ceba55a08ff6cfd112d4
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-08 00:05:32 +0530

    Added barebones get for tracks. Updated typo for use and also renamed trackSchema to Track model.

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requiresAuth = require("../middlewares/requiresAuth");
 
-const trackSchema = mongoose.model("Track");
+const Track = mongoose.model("Track");
 
 const router = express.Router();
 
-router.use = requiresAuth;
+router.use(requiresAuth);
+
+router.get("/tracks", (req, res) => {
+    
+})
