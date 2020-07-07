9d78f8176f97240da543607b1d201b7d0beb89ad
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-08 00:25:04 +0530

    Added route for tracks post.
    Update:
    1. Gets name and locations from body.
    2. Creates track from name, locations and user id and if not sends creation error.
    3. Saves track.
    4. Returns track as response.

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -13,6 +13,18 @@ router.get("/tracks", async (req, res) => {
     const tracks = await Track.find({ userId: req.user._id });
 
     res.send({ tracks });
+});
+
+router.post("/tracks", async (req, res) => {
+    const { name, locations } = req.body;
+    try {
+        const track = new Track({ name, locations, userId: req.user._id });
+        await track.save();
+        res.send({ track })
+    } catch (err) {
+        res.status(422).send({ error: err.message });
+    }
+    
 })
 
 module.exports = router;
