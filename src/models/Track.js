ad089fa21e7324cc7bf60c2cfda7cae7c3888df7
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-07 23:39:19 +0530

    Defined schema for Track.

--- src/models/Track.js
+++ src/models/Track.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
 
+const trackSchema = mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+
+    name: {
+        type: String,
+        default: ""
+    },
+
+    locations: [pointSchema]
+})
