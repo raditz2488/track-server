0d26262c3bfc4d2775ee4c43a92bade9b7d6aec6
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-08 00:09:11 +0530

    Inside get tracks added code to get tracks from mongoose. Response sets the tracks. Also router is exported.

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requiresAuth = require("../middlewares/requiresAuth");
+const { route } = require('./authRoutes');
 
 const Track = mongoose.model("Track");
 
@@ -9,5 +10,9 @@ const router = express.Router();
 router.use(requiresAuth);
 
 router.get("/tracks", (req, res) => {
-    
+    const tracks = await Track.find({ userId: req.user._id });
+
+    res.send({ tracks });
 })
+
+module.exports = router;
