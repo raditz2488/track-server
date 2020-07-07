bab5f60d2bf3e2c95de743c9ebbf1b33c9e1c12e
Rohan Bhale <rohanbhale@Rohans-MacBook-Pro.local>
2020-07-07 21:58:10 +0530

    Added code to post of signin to get user for the email. If no user is found respond with status 404 and error message.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -27,6 +27,11 @@ router.post('/signin', async (req, res) => {
     if (!email || !password) {
         res.status(422).send({ "error": "Email and password are required." });
     }
+
+    const user  = await User.findOne({ email });
+    if (!user) {
+        res.status(404).send({ "error": "Invalid email or password." })
+    }
 })
 
 module.exports = router;
